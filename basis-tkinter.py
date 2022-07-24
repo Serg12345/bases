4283fbaaee1f20209cc2060bcb4be60876e51082
sergey <zxsergq@gmail.com>
2022-07-24 16:38:30 +0300

    Библиотека Tkinter. Способы задания опций кнопке

--- basis-tkinter.py
+++ basis-tkinter.py
@@ -14,6 +14,17 @@ def hello():
 # button_1 = Button(window, text = 'Кнопка 1', command = hello, width = 10, height = 2, bd = 10, font = 'Arial 10 bold')
 button_1 = Button(window, text = 'Кнопка 1', padx = 5, pady = 5, bd = 10, activebackground = 'yellow', activeforeground = 'orange', bg = 'green', fg = 'white')
 
+button_2 = Button(window)
+button_2.config(text = 'Кнопка 2', padx = 5, pady = 5, bd = 10, activebackground = 'yellow', activeforeground = 'orange', bg = 'green', fg = 'white')
+
+button_3 = Button(window)
+button_3['text'] = 'Кнопка 3'
+button_3['padx'] = 5
+button_3['pady'] = 5
+button_3['bd'] = 10
+
 button_1.pack()
+button_2.pack()
+button_3.pack()
 
 window.mainloop()
